eb64983c0ef5d24e62f2105d8ed7f6540dd9001e
hmia334 <h2756848729@163.com>
2026-03-29 14:39:06 +0800

    feat: 测试

---
+++ src/main/java/com/cici/ccaiagent/CcAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.cici.ccaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cici/ccaiagent/CcAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cici.ccaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
